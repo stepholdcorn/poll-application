6e93bcfa34060d138ed22e8dfbe86ed17d904c8a
steph <stephieoldcorn@gmail.com>
2015-03-27 14:01:13 +0000

    Implements the render shortcut for the index page

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from polls.models import Question
 
 # Create your views here.
 def index(request):
   lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-  output = ', '.join([p.question_text for p in lastest_question_list])
-  return HttpResponse(output)
+  context = { 'lastest_question_list': lastest_question_list }
+  return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
   return HttpResponse('You are looking at question %s.' % question_id)
